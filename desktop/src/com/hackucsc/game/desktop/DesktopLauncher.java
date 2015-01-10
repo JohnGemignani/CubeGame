a85ded1854330ab091b2bf87460262ec46ee483d
John Gemignani <jrgemigna@ucsc.edu>
2015-01-10 02:00:25 -0800

    Initial commit

---
+++ desktop/src/com/hackucsc/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hackucsc.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hackucsc.game.CubeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CubeGame(), config);
+	}
+}
